3cac2d400421c4914706f4f8dbbadc1c90d90822
Robert <r.powelliv@gmail.com>
2020-12-29 19:52:09 -0500

    Portfolio

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class footer extends Component {
+  render() {
+    return (
+      <div className="site-footer" id="footer">
+        <div className="container">
+          <span>Robert Powell</span>
+        </div>
+      </div>
+    );
+  }
+}
